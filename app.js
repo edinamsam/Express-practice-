1fec6f9fe880ecda2058e1002863170f5cd4fe53
Samuel Adzoho <edinamsam4@gmail.com>
2026-08-29 18:23:14 +0000

    adding url functionality in server

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ const { readFileSync } = require("fs");
 
 //get file
 const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/style.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
-  //home page
+  // home page
   if (url == "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
     res.end();
   }
 
-  //about page
+  // about page
   else if (url == "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>about page</h1>");
     res.end();
   }
 
-  //404
+  // styles
+  else if (url == "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+
+  // image/log
+  else if (url == "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  }
+
+  // 404
   else {
     res.writeHead(404, { "content-type": "text/html" });
     res.write("<h1>page not found</h1>");
