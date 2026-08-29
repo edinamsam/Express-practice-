c73eb6ad9e76ebb8fea7644e2b4a0d6042549955
Samuel Adzoho <edinamsam4@gmail.com>
2026-08-29 15:44:09 +0000

    assessing an html file on a server

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+const homePage = readFileSync("./index.html");
+
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   //home page
   if (url == "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>home page</h1>");
+    res.write(homePage);
     res.end();
   }
 
