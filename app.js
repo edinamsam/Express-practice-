70ff2fa1e4fada5c8eacd9466bb38336e824f55c
Samuel Adzoho <edinamsam4@gmail.com>
2026-08-30 15:51:03 +0000

    responding to a request to an unavailable resource

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get("/", (req, res) => {
   res.send("Home page");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.all("*", (req, res) => {
+  res.send("<h1>resource not found</h1>");
+});
+
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
 });
