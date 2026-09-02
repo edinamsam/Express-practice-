dbe6efe311983b4fa7f055ebd38ab4885e156fe7
Samuel Adzoho <edinamsam4@gmail.com>
2026-09-02 11:47:56 +0000

    housekeeping changes

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// setup static and middleware
 app.use(express.static("./public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
 
 app.use((req, res) => {
   res.status(400).send("resource not found");
